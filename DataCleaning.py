db98012da4ba23cdab5f6f151ff541f4e5ab7250
Angkirat Sandhu <angkirat@gmail.com>
2021-11-29 19:51:24 +0000

    Added DataFile and Data cleaning file

---
+++ DataCleaning.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+import itertools
+
+def one_hot_single(column:pd.Series) -> pd.DataFrame:
+    oneHot = OneHotEncoder(handle_unknown="ignore")
+    col = column.values.reshape((-1,1))
+    encodedCol = oneHot.fit_transform(col)
+    return pd.DataFrame(encodedCol, columns=oneHot.categories_[0])
+
+def one_hot_multi(columns:pd.Series, colsep:str=',') -> pd.DataFrame:
+    valueCol = columns.apply(lambda x: [val.strip() for val in x.split(colsep)])
+    distinctVal = list(set(itertools.chain(*list(valueCol))))
+    outputdf = pd.DataFrame()
+    for val in distinctVal:
+        outputdf[val] = valueCol.apply(lambda x: val in x)
+    return outputdf
+    
+
+def binary_column(columns: pd.Series) -> pd.Series:
+    df = pd.get_dummies(columns)
+    return df.iloc[:, 1]
---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+DF = pd.read_excel('FlixGem.com Dataset - Latest Netflix data with thousands of attributes.xlsx', sheet_name='FlixGem.com dataset')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    pass
