812347e65811db2540e93f0b37291c450c6659cf
Akul Rastogi <akulrastogi92@gmail.com>
2021-11-30 18:27:15 +0000

    YouTube scrape logic added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from scrape import get_stats
 
 DF = pd.read_excel('FlixGem.com Dataset - Latest Netflix data with thousands of attributes.xlsx', sheet_name='FlixGem.com dataset')
 
 def main():
-    pass
+	yt_stats = get_stats(DF["TMDb Trailer"][DF["Trailer Site"] == "YouTube"].tolist())
+	print(yt_stats)
 
 if __name__ == '__main__':
     main()
