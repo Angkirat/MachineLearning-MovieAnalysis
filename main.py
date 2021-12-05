1d987eea19e8fad9473f2929fdaea5c019ac4639
Angkirat Sandhu <angkirat@gmail.com>
2021-12-05 11:55:28 +0000

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from pandas.core.reshape.merge import merge
 import DataCleaning as dc
-from scrape import get_stats
+# from scrape import get_stats
 
 dataCleaning = {
     'SingleValue_onehotEncoding': ['View Rating','Runtime']
@@ -33,11 +34,18 @@ def data_cleaning_operation(inputDF: pd.DataFrame):
     return inputDF
 
 def main():
-	DF = pd.read_excel('FlixGem.com Dataset - Latest Netflix data with thousands of attributes.xlsx',sheet_name='FlixGem.com dataset')
-	yt_stats = get_stats(DF["TMDb Trailer"][DF["Trailer Site"] == "YouTube"].tolist())
-	yt_stats.to_csv('yt_stats.csv')
-	pass
+    DF = pd.read_excel('FlixGem.com Dataset - Latest Netflix data with thousands of attributes.xlsx',sheet_name='FlixGem.com dataset')
+    yt_stats = pd.read_csv('yt_stats.csv').iloc[:, 1:]
+    complete_DF = DF.merge(yt_stats, left_on='TMDb Trailer', right_on='trailer_link')
+    
+    print(DF.shape)
+    print(yt_stats.shape)
+    print(complete_DF.shape)
+
+
+
 
 if __name__ == '__main__':
     main()
     pass
+
